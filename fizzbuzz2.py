9d79093659785db170689622456ee63394814090
rtuita23 <rktuita23@hotmail.com>
2015-01-13 05:06:39 +0000

    latest version

--- fizzbuzz2.py
+++ fizzbuzz2.py
@@ -2,32 +2,19 @@ import sys
 
 n = 0
 
-if len(sys.argv) == 1: # handle when no user input at command line
+try:  # handle user input at command line
+  n = int(sys.argv[1])
+except:
   while True:
-    try:
-      n = int(raw_input("Please enter a number! ")) 
-    except ValueError: 
-      print("oops, not a number")
+    try: 
+      n = int(raw_input("Please enter a number: "))
+    except ValueError:
       continue
     else:
       break
-else: # handle user input at command line
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-      print("Oops, not a number")
-      while True:
-        try:
-          n = int(raw_input("Please enter a number! "))
-        except ValueError:
-          print("Oops, not a number")
-          continue
-        else:
-          break
-      
-print("Fizz buzz counting up to " + str(n)) # print the number entered at command line
-
-for num in range(1,n+1): # looping thru range of numbers and printing Fizz and Buzz
+  
+print("Fizz buzz counting up to " + str(n))  # print the number entered at command line
+for num in range(1,n+1):  # looping thru range of numbers and printing Fizz and Buzz
   if num % 3 == 0 and num % 5 == 0:
     print("Fizz Buzz")
   elif num % 3 == 0:
